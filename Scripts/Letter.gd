424a119a68172e3beb5fa2d31727a2867a2842ab
Connor Settle <csettle@stanford.edu>
2025-02-05 09:44:04 -0800

    made the letter look fancier

--- Scripts/Letter.gd
+++ Scripts/Letter.gd
@@ -3,17 +3,13 @@ extends Node2D
 class_name Letter
 
 var content: String = "" # Defaults to "", the EOF object for words
-@export var text_label: RichTextLabel
-@export var text_background: RichTextLabel
+@export var text_label: Label
 @onready var machineManager: MachineManager = get_node("/root/Main Scene/MachineManager")
 
 func set_letter(content: String) -> void:
 	if len(content) >= 1:
 		self.content = content[0]
-		self.text_label.clear()
-		self.text_label.add_text(content)
-		self.text_background.clear()
-		self.text_background.add_text(content)
+		self.text_label.text = self.content
 
 func _to_string() -> String:
 	return content
